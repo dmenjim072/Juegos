24fdf2fb013a77852da4fa518819c5034a38430c
David <dmenjim072@g.educaand.es>
2022-11-06 22:24:11 +0100

    terminado

--- src/main/java/juegos/ParesNones.java
+++ src/main/java/juegos/ParesNones.java
@@ -2,68 +2,146 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package juegos;
 
+import java.util.Random;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author david
- */
 public class ParesNones {
 
     //main
     public static void main(String[] args) {
+
+        //Declaramos variables 
+        int menuDedos = 0;
+        int numMaquina = 0;
+        int sumaDedos = 0;
+        String menuInicial = "";
+        String menuParesNones = "";
+        final int MINIMO = 1;
+        final int MAXIMO = 5;
+        
+        
+        //Hacemos el bucle menu 1
+        do{
+       
+            //Mostramos el menu inicial  
+            menuInicial = mostrarMenuInicial();
+
+            //Hacemos una condicion para que entre al siguiente menu si escribimos "jugar"
+            if (menuInicial.equalsIgnoreCase("jugar")) {
+                           
+                //llamamos al metodo para elegir pares o nones
+                menuParesNones = mostrarMenuParesNones(menuParesNones);
+                
+                //hacemos la condicion para que avance al siguiente menu
+                if (menuParesNones.equalsIgnoreCase("pares")
+                        ||menuParesNones.equalsIgnoreCase("nones")){
                     
-        mostrarMenuInicial();
-        
-        if (menuInicial.equalsIgnoreCase("jugar")){
-            menuParesNones();
-        }
-        
+                    //llamamos al metodo para elegir el numero de dedos
+                    menuDedos = mostrarMenuDedos(menuDedos);
+                    
+                    //hacemos la condicion para que avance
+                    if (menuDedos>=1 && menuDedos<=5){
+                                            
+                        //llamamos al metodo para que la maquina elija dedos
+                        numMaquina = dedosMaquina(MINIMO, MAXIMO);
+                        
+                        //llamamos al metodo para que sume los dedos de ambas manos
+                        sumaDedos = totalDedos(menuDedos, numMaquina);
+                        
+                        //llamamos al metodo para que muestre el resultado
+                        mostrarResultado(menuParesNones, sumaDedos);
+                    
+                    }
+                }
+            }
+            
+        //se repite hasta que ponga "salir" bucle menu 1
+        }while(!menuInicial.equalsIgnoreCase("salir"));  
     }
     
-    //metodo para mostrar el menu inicial
-    public static String mostrarMenuInicial(){
-        
+        //metodo para mostrar el menu inicial
+    public static String mostrarMenuInicial() {
+
         String menuInicial;
         
-        do{
         menuInicial = JOptionPane.showInputDialog("Introduce JUGAR para entrar \n"
                 + "o introduce SALIR para salir del programa");
-        
-            switch (menuInicial){
-            case "jugar": 
-                
-                break;
-            case "salir":
-                JOptionPane.showMessageDialog(null, "Hasta luego!");
-                break;
-        }
-                
-        }while(!menuInicial.equalsIgnoreCase("salir"));
+
         return menuInicial;
+        
+    }
+
+    //metodo para eleccion de pares o nones con bucle
+    public static String mostrarMenuParesNones(String menuParesNones) {
+
+        do{
+            menuParesNones = JOptionPane.showInputDialog("Introduce PARES si quieres ser pares \n"
+                + "Introduce NONES si quieres ser nones \n"
+                + "Introduce SALIR si quieres salir");
+         
+            return menuParesNones;
+        }while(!menuParesNones.equalsIgnoreCase("salir"));
+    }
+
+    //metodo para eleccion dedos jugador
+    public static int mostrarMenuDedos(int menuDedos) {
+
+        do{
+            String menuDedosString = JOptionPane.showInputDialog
+            ("Introduce el numero de dedos (1 - 5): ");
+            menuDedos = Integer.parseInt(menuDedosString);
+
+            return menuDedos;
+        }while(menuDedos<1 || menuDedos>5);
     }
     
-    //metodo para eleccion de pares o nones
-    public static void menuParesNones(){
+    //metodo para dedos maquina aleatorio
+    public static int dedosMaquina(final int MINIMO, final int MAXIMO){
         
-        String menuParesNones;
+        Random numAleatorio = new Random();
+              
+        int numMaquina = numAleatorio.nextInt(MINIMO, MAXIMO+1);
         
-        do{menuParesNones = JOptionPane.showInputDialog("Introduce PARES si quieres ser pares \n"
-                + "Introduce NONES si quieres ser nones"
-                + "Introduce SALIR si quieres salir");
+        return numMaquina;
+    }
+    
+    //metodo para sumar dedos de ambas manos
+    public static int totalDedos(int numMaquina, int menuDedos){
         
-            switch (menuParesNones){
-                case "pares":
-                    break;
-                case "nones":
-                    break;
-            }    
+        int sumaDedos;
         
-        }while (!menuParesNones.equalsIgnoreCase("salir"));{
+        sumaDedos = numMaquina + menuDedos;
+        
+        return sumaDedos;
+    }
+    
+    //metodo para mostrar el resultado
+    public static void mostrarResultado(String menuParesNones, int sumaDedos){
+        
+        //si es pares
+        if(menuParesNones.equalsIgnoreCase("pares")){
+            if (sumaDedos % 2 == 0){
+            JOptionPane.showMessageDialog(null, "Has ganado! La suma de los dedos es: " + sumaDedos);
+            
+            }else{
+            JOptionPane.showMessageDialog(null, "Has perdido! La suma de los dedos es: " + sumaDedos);
+            }
         
         }
+    
+        //si es nones
+        if(menuParesNones.equalsIgnoreCase("nones")){
+            if (sumaDedos % 2 == 1){
+            JOptionPane.showMessageDialog(null, "Has ganado! La suma de los dedos es: " + sumaDedos);
+            
+            }else{
+            JOptionPane.showMessageDialog(null, "Has perdido! La suma de los dedos es: " + sumaDedos);
+            }
+        
+        }
+    
     }
-}
+    
+    }
